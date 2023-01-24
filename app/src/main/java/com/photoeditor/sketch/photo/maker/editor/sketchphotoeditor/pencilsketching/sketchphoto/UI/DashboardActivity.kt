1761fee5cbb3266ce3b2fab6d5f61867b01ff82f
Mohib Rehman <mohib.rehman@proact.net>
2023-01-24 11:00:11 +0000

    remove old gallery code

--- app/src/main/java/com/photoeditor/sketch/photo/maker/editor/sketchphotoeditor/pencilsketching/sketchphoto/UI/DashboardActivity.kt
+++ app/src/main/java/com/photoeditor/sketch/photo/maker/editor/sketchphotoeditor/pencilsketching/sketchphoto/UI/DashboardActivity.kt
@@ -1,10 +1,8 @@
 package com.photoeditor.sketch.photo.maker.editor.sketchphotoeditor.pencilsketching.sketchphoto.UI
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.content.*
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.DisplayMetrics
@@ -14,14 +12,12 @@ import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.esafirm.imagepicker.features.*
 import com.esafirm.imagepicker.model.Image
 import com.photoeditor.sketch.photo.maker.editor.sketchphotoeditor.pencilsketching.R
-import com.photoeditor.sketch.photo.maker.editor.sketchphotoeditor.pencilsketching.sketchphoto.gallery.MediaChooser
-import com.photoeditor.sketch.photo.maker.editor.sketchphotoeditor.pencilsketching.sketchphoto.constant.AppConstant
 import com.photoeditor.sketch.photo.maker.editor.sketchphotoeditor.pencilsketching.sketchphoto.UI.pencil.ImageRemakeActivity
+import com.photoeditor.sketch.photo.maker.editor.sketchphotoeditor.pencilsketching.sketchphoto.constant.AppConstant
 
 class DashboardActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -43,8 +39,7 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         context = this
-        val imageIntentFilter = IntentFilter(MediaChooser.IMAGE_SELECTED_ACTION_FROM_MEDIA_CHOOSER)
-        registerReceiver(imageBroadcastReceiver, imageIntentFilter)
+
         val displaymetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displaymetrics)
         widthPixel = displaymetrics.widthPixels
@@ -55,11 +50,9 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btnPrivacy -> gotothisLink(AppConstant.privacyLink)
             R.id.btnRateUs -> gotothisLink("market://details?id=$packageName")
             R.id.btnMoreApps -> gotothisLink(AppConstant.moreAppsLink)
-            R.id.btnStart -> pickImageFromGallery()
+            R.id.btnStart -> pickImageWithLib()
             R.id.btnGallery -> startActivity(Intent(context, MyGalleryActivity::class.java))
-            R.id.btnPickImage -> {
-                pickImageWithLib()
-            }
+          
         }
     }
 
@@ -83,52 +76,11 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun pickImageFromGallery() {
-        /* if (Build.VERSION.SDK_INT >= 23) {
-             if (checkPermission()) {
-                 MediaChooser.setSelectionLimit(20)
-                 val intent = Intent(this@DashboardActivity, ChoosePhotoFragmentActivity::class.java)
-                 startActivity(intent)
-             } else {
-                 requestPermission() // Code for permission
-             }
-         } else {
-             MediaChooser.setSelectionLimit(20)
-             val intent = Intent(this@DashboardActivity, ChoosePhotoFragmentActivity::class.java)
-             startActivity(intent)
-         }*/
-    }
 
     private fun gotothisLink(url: String) {
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         startActivity(intent)
-    }
-
-    private fun checkPermission(): Boolean {
-        val result = ContextCompat.checkSelfPermission(
-            this@DashboardActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-        return result == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this@DashboardActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-        ) {
-            Toast.makeText(
-                this@DashboardActivity,
-                "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.",
-                Toast.LENGTH_LONG
-            ).show()
-        } else {
-            ActivityCompat.requestPermissions(
-                this@DashboardActivity,
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_CODE
-            )
-        }
     }
 
 
@@ -141,9 +93,7 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener {
             (getSystemService("activity") as ActivityManager).deviceConfigurationInfo.reqGlEsVersion >= 0x20000
         if (!flag) {
             Toast.makeText(
-                applicationContext,
-                "Editor is not supported in this device.",
-                Toast.LENGTH_SHORT
+                applicationContext, "Editor is not supported in this device.", Toast.LENGTH_SHORT
             ).show()
 
             return
