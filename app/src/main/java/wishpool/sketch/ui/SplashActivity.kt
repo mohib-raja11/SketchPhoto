d58ff0f6d41d338ac26847f8102ae8110ff712e0
Mohib Rehman <mohib.rehman@proact.net>
2023-05-30 00:07:24 +0100

    splash screen added

---
+++ app/src/main/java/wishpool/sketch/ui/SplashActivity.kt
@@ -0,0 +1,24 @@
+package wishpool.sketch.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import wishpool.sketch.databinding.ActivitySplashBinding
+import wishpool.sketch.utils.openNextActivity
+
+class SplashActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            openNextActivity(DashboardActivity::class.java)
+        }, 300)
+    }
+}
